1210be7d780d6e3173de0c497e2301e7126790ca
kola pundarikakshayya <saicharan.kola@cigniti.com>
2026-02-28 13:07:39 +0530

    my commit

---
+++ ExecPrg/src/test/java/Exeprgm.java
@@ -0,0 +1,18 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class Exeprgm {
+
+    public static void main(String[] args) {
+
+
+        String str = "saicharan";
+        StringBuilder sb = new StringBuilder(str);
+
+        System.out.println(sb.reverse().toString().trim());
+
+
+    }
+
+}
